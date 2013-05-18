756271b88c34508df43134f2108e4037be9236b3
255 <5926535+github@gmail.com>
2013-05-18 15:58:17 -0400

    small change

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ app.get('/', function(req, res){
 });
 
 app.listen(3000);
-//console.log("DRAWesome server started on port %d in %s mode", app.address().port, app.settings.env);
+console.log("DRAWesome server started on port %d in %s mode", app.address().port, app.settings.env);
 
 var clients = [];
 var count = 0; // How many connected clients
@@ -61,15 +61,20 @@ io.sockets.on('connection', function(socket)
       
       console.log("Client %s has joined. We now have %d artists on the server.", client.name, count);
 
-      socket.emit('test', client.name);
+      if (count > 1) {
+         socket.emit('test', "Greetings, " + client.name + "! You have joined\n" + listClients() + ".");
+      } else {
+         socket.emit('test', "Greetings, " + client.name + "!");
+      }
+      
       socket.emit('connect_1', client);
       io.sockets.emit('load', clients);
    });
    
    socket.on('disconnect', function() {
       var idx = clientIdx(socket.id);
-      console.log("The client \"" + clients[idx].name + "\" has left the building.");
       count--;
+      console.log("The client \"" + clients[idx].name + "\" has left the building. Only " + count + "remain.");
       
       for (; idx < count; idx++) {
          clients[idx] = clients[idx + 1]
@@ -88,5 +93,8 @@ function listClients() {
    str = "";
    for (i = 0; i < count; i++) {
       str += clients[i].name;
+         if (i < count - 1)
+            str += ", ";
    }
+   return str;
 }
