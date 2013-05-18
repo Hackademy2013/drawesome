6d1b5a094342eab3f259e9d5de997c17a83e433d
255 <5926535+github@gmail.com>
2013-05-18 15:14:18 -0400

    updated server code

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
-/**
- * Module dependencies.
- */
+/**************************************\
+ *              DRAWsome               *
+ *                                     *
+ *        by James Kilian,             *
+ *           Wyatt Hepler              *
+ *           Kavya Nagaraj             *
+ *                                     *
+\**************************************/
 
-var express = require('express');
-var io = require('socket.io');
+// Dependencies
+var express = require('express'),
+    io      = require('socket.io');
 
 var app = module.exports = express.createServer();
 
@@ -13,111 +19,74 @@ var io = io.listen(app);
 
 io.set('log level', 1); // Turn off annoying polling
 app.configure(function() {
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(__dirname + '/public'));
+   app.set('views', __dirname + '/views');
+   app.set('view engine', 'jade');
+   app.use(express.bodyParser());
+   app.use(express.methodOverride());
+   app.use(app.router);
+   app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
+   app.use(express.errorHandler({ dumpExceptions: true, showStack: true })); 
 });
 
 app.configure('production', function(){
-  app.use(express.errorHandler()); 
+   app.use(express.errorHandler()); 
 });
 
 // Routes
 
 app.get('/', function(req, res){
-  res.render('index');
+   res.render('index');
 });
 
 app.listen(3000);
 console.log("DRAWesome server started on port %d in %s mode", app.address().port, app.settings.env);
 
 var clients = [];
-var i = 0; // How many connected clients
+var count = 0; // How many connected clients
 
 io.sockets.on('connection', function(socket)
 {
-  console.log("We had a connection from socket id: " + socket.id);
-  
-  socket.on('client_connected', function(player)
-  {
-    player.id = socket.id;
-   console.log("Client connected: " + socket.id);
-
-    clients[i] = player;
-    i++;
-    
-   console.log("Player " + player.name + " has joined. " + 
-               "We now have " + i + " artists on the server.");
-	
-   socket.emit('test', player.name);
+   console.log("Socket id: " + socket.id + " wants to join the fun.");
    
-    socket.emit('connect_1', player);
-    io.sockets.emit('load', clients);
-  });
-  
-  socket.on('process_move', function(coords)
-  {
-    var n = 0;
-    coords = coords.replace("#",'');
-	
-    
-    // ToDo: Send in players mark instead of ugly loop
-    while (n < clients.length)
-    {
-      if (clients[n].id == socket.id)
-      {
-        grid[coords] = clients[n].mark;
-      }
-      n++;
-    }
-    
-    // Update clients with the move
-    io.sockets.emit('mark', coords);
-    
-    // Win check
-
-  });
-  
-  socket.on('disconnect', function()
+   socket.on('client_connected', function(client)
    {
-     var j = 0;
-     var n = 0;
-     var tmp = [];
+      client.id = socket.id;
 
-     while (n < clients.length)
-     {
-       if (clients[j].id == socket.id)
-       {
-         if(clients[j].mark == 'o')
-         {
-           xo = 'o';
-           o = false;
-         }
-         if(clients[j].mark == 'x')
-         {
-           xo = 'x';
-         }
-     	   n++;
-     	 }
-     	 
-     	 if (n < clients.length)
-     	 {
-     	   tmp[j] = clients[n];
-     	   j++;
-     	   n++;
-     	  }
-     	}
-     	
-     	clients = tmp;
-     	i = j;
+      // save this client
+      clients[count] = client;
+      count++;
+      
+      console.log("Client %s has joined. We now have %d artists on the server.", client.name, count);
+
+      socket.emit('test', client.name);
+      socket.emit('connect_1', client);
       io.sockets.emit('load', clients);
    });
-  
+   
+   socket.on('disconnect', function() {
+      var idx = clientIdx(socket.id);
+      console.log("The client \"" + clients[idx].name + "\" has left the building.");
+      count--;
+      
+      for (; idx < count; idx++) {
+         clients[idx] = clients[idx + 1]
+      }
+   });
 });
+
+// Tools
+function clientIdx(id) {
+   for (i = 0; i < count; i++)
+      if (clients[i].id === id)
+         return i;
+}
+
+function listClients() {
+   str = "";
+   for (i = 0; i < count; i++) {
+      str += clients[i].name;
+   }
+}
